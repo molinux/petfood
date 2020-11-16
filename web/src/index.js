45b69c9899d19de4e7a684be68cfb3697860fa05
Molinux <cybercrowbr@gmail.com>
2020-11-16 10:48:14 -0300

    :sparkles: Page for stores finished.

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Cadastro from './pages/cadastro';
 import Checkout from './pages/checkout';
+import Petshop from './pages/petshop';
 
 import './styles/global.css';
 
@@ -10,7 +11,8 @@ import './styles/global.css';
 ReactDOM.render(
   <React.StrictMode>
     {/* <Cadastro /> */}
-    <Checkout />
+    {/* <Checkout /> */}
+    <Petshop />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ web/src/pages/petshop/index.js
@@ -0,0 +1,44 @@
+import Header from '../../components/header';
+import Product from '../../components/product/card';
+
+import './styles.css';
+
+const Petshop = () => {
+  return (
+    <div className="h-100">
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <img 
+              src="https://www.petlove.com.br/static/uploads/banner_image/image/4304/logo-petlove-push.png" 
+              alt="petlove" 
+              className="img-fluid petshop-image"
+            />
+            <b>Petlove</b>
+            <div className="petshop-infos">
+              <span className="mdi mdi-star"></span>
+              <text>
+                <b>2,8</b>
+              </text>
+              <span className="mdi mdi-cash-usd-outline"></span>
+              <text>$$$</text>
+              <span className="mdi mdi-crosshairs-gps"></span>
+              <text>2,9Km</text>
+            </div>
+            <label className="badge badge-primary">Frete grátis</label>
+          </div>
+          <div className="col-10">
+            <h5>Produtos</h5>
+            <br />
+            <div className="row">
+            {[1,2,3,4,5,6,7,8,9].map(p => <Product />)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Petshop;
