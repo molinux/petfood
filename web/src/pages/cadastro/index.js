fc095bde89d4a8089a1d323437f740e76fe2532f
Molinux <cybercrowbr@gmail.com>
2020-12-02 19:36:14 -0300

    :construction: Initiating with Redux implementation

--- web/src/pages/cadastro/index.js
+++ web/src/pages/cadastro/index.js
@@ -1,9 +1,35 @@
 // import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 import Header from '../../components/header';
 
 import Illustration from '../../assets/illustration.png'
 
 const Cadastro = () => {
+
+  const dispatch = useDispatch();
+
+  const [customer, setCustomer] = useState({
+    "external_id": new Date().getTime().toString(),
+    "name": '',
+    "type": "individual",
+    "country": "br",
+    "email": '',
+    "documents": [
+      {
+        "type": "cpf",
+        "number": '',
+      },
+    ],
+    "phone_numbers": [''],
+    "birthday": '',
+  });
+
+  const goToCheckout = () => {
+    dispatch({ type: 'SET_CUSTOMER', customer });
+  }
+
   return (
     <div className="container-fluid h-100 bg-primary">
       <Header whiteVersion hideCart/>
@@ -19,29 +45,51 @@ const Cadastro = () => {
             type="text" 
             className="form-control form-control-lg  mb-3" 
             placeholder="Nome completo"
+            onChange={(e) => {
+              setCustomer({ ...customer, name: e.target.value });
+            }}
           />
           <input 
             type="text" 
             className="form-control form-control-lg  mb-3" 
             placeholder="E-mail"
+            onChange={(e) => {
+              setCustomer({ ...customer, email: e.target.value });
+            }}
           />
           <input 
             type="text" 
             className="form-control form-control-lg  mb-3" 
             placeholder="Telefone"
+            onChange={(e) => {
+              setCustomer({ ...customer, phone_numbers: [e.target.value] });
+            }}
           />
           <input 
             type="text" 
             className="form-control form-control-lg  mb-3" 
             placeholder="CPF"
+            onChange={(e) => {
+              setCustomer({ ...customer, 
+                documents: [
+                  {
+                    type: 'cpf',
+                    number: e.target.value,
+                  },
+                ], 
+              });
+            }}
           />
           <input 
             type="date" 
             className="form-control form-control-lg  mb-3" 
             placeholder="Data de Nascimento"
+            onChange={(e) => {
+              setCustomer({ ...customer, birthday: e.target.value });
+            }}
           />
 
-          <button className="btn btn-lg btn-block btn-secondary">
+          <button onClick={() => goToCheckout()} className="btn btn-lg btn-block btn-secondary">
             Finalizar Pedido
           </button>
         </div>
